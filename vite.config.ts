ab4396ca3b59130060e279b147fd13dfacdbe1c6
EmmyDan1 <emmydan68@gmail.com>
2025-12-22 18:45:47 +0100

    staged

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  base: './',
+  server: {
+    port: 3000,
+  }
 })
